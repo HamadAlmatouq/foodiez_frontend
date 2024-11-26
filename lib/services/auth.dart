ba00ccaf974733f27df50a2a2a93364215531f19
eng-batol <batol.alashwak@gmail.com>
2024-11-26 10:04:23 +0300

    auth servce

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,16 +1,18 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:foodiez_frontend/models/user.dart';
 import 'package:foodiez_frontend/services/client.dart';
 
 class AuthServices {
+  get http => null;
 
   Future<String> signup({required User user}) async {
     late String token;
     try {
       // print('Signing up');
       // print(user.toJson());
-      Response response =
-          await Client.dio.post('/signup', data: user.toJson());
+      Response response = await Client.dio.post('/signup', data: user.toJson());
       token = response.data["token"];
       // print(response.data);
       return token;
@@ -20,16 +22,34 @@ class AuthServices {
     }
   }
 
+//   Future<String> signin({required User user}) async {
+//     late String token;
+//     try {
+//       Response response = await Client.dio.post('/signin', data: user.toJson());
+//       token = response.data["token"];
+//       print(token);
+//     } on DioException catch (error) {
+//       print(error);
+//     }
+//     return token;
+//   }
+// }
   Future<String> signin({required User user}) async {
-    late String token;
-    try {
-      Response response =
-          await Client.dio.post('/signin', data: user.toJson());
-      token = response.data["token"];
-      print(token);
-    } on DioException catch (error) {
-      print(error);
+    final response = await http.post(
+      Uri.parse('http://localhost:8000/api/users/signin'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'username': user.username,
+        'password': user.password,
+      }),
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['token'];
+    } else {
+      throw Exception('Failed to sign in');
     }
-    return token;
   }
 }
